14f26adb970d3fc42f33559a29d9ad17ddf62162
a-nester <a_nester@ukr.net>
2024-10-29 23:02:22 +0200

    add/company-form

---
+++ src/app/components/add-company-button.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useState } from 'react';
+import Button from './button';
+import CompanyFormModal from './company-form-modal';
+
+export default function AddCompanyButton() {
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setShow(true)}>Add company</Button>
+      <CompanyFormModal
+        onSubmit={console.log}
+        show={show}
+        onClose={() => setShow(false)}
+      />
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import StatusLabel, { Status } from './components/status-label';
+import { headers } from 'next/headers';
+import AddCompanyButton from './components/add-company-button';
 
 export default function Home() {
+  console.log(headers());
+
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Active</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Active</StatusLabel>
+      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
+      <AddCompanyButton />
     </main>
   );
 }
