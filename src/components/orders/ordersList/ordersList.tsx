c07414f03294d8012940d2d96437ea98f2b4d594
a-nester <a_nester@ukr.net>
2025-02-10 00:27:00 +0200

    style/mobile

--- src/components/orders/ordersList/ordersList.tsx
+++ src/components/orders/ordersList/ordersList.tsx
@@ -5,6 +5,16 @@ import Order from '../order/order';
 import CommonMultiSelect from '../../CommonComponents/CommonMultiSelect/CommonMultiSelect';
 import { useEffect, useState } from 'react';
 
+const DateSeparator = ({ date }: { date: string }) => (
+  <div className="flex items-center my-4">
+    <div className="flex-grow border-t border-gray-700"></div>
+    <span className="px-3 py-1 text-sm font-semibold bg-gray-800 text-gray-300 rounded-full mx-2">
+      {date}
+    </span>
+    <div className="flex-grow border-t border-gray-700"></div>
+  </div>
+);
+
 export default function OrdersList() {
   const { shops, addStores, fetchOrders } = useStore();
   const STORE_IDS = ['AvtoKlan', 'AutoAx', 'iDoAuto', 'ToAuto'];
@@ -24,18 +34,37 @@ export default function OrdersList() {
     return () => clearInterval(interval);
   }, [storesList, fetchOrders]);
 
+  const uniqueDates = [
+    ...new Set(orders.map((order) => order.date_created.split('T')[0])),
+  ];
+
+  const formatDate = (dateCreated: string) => {
+    return dateCreated.split('T')[0];
+  };
+
   return (
     <section className="flex flex-col gap-2 bg-gray-100">
       <CommonMultiSelect values={storesList} setValues={setStoresList} multiple>
         {STORE_IDS}
       </CommonMultiSelect>
-      <ul className="flex flex-col gap-2">
-        {orders.map((elem) => (
-          <li key={elem.id}>
-            <Order elem={elem} />
-          </li>
+      <div className="flex flex-col gap-2">
+        {uniqueDates.map((date) => (
+          <div key={date}>
+            {' '}
+            {/* Замість <li> тепер <div> */}
+            <DateSeparator date={date} />
+            <ul>
+              {orders
+                .filter((order) => formatDate(order.date_created) === date)
+                .map((elem) => (
+                  <li key={elem.id}>
+                    <Order elem={elem} />
+                  </li>
+                ))}
+            </ul>
+          </div>
         ))}
-      </ul>
+      </div>
     </section>
   );
 }
