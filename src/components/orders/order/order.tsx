629604ae1eac7bb11901f9430e4864aebc5b889d
a-nester <a_nester@ukr.net>
2025-02-10 22:47:10 +0200

    style/mobile

--- src/components/orders/order/order.tsx
+++ src/components/orders/order/order.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import styles from './order.module.css';
 import { Order as OrderType } from '@/types/types';
+import { HiOutlinePercentBadge, HiOutlineShoppingCart } from 'react-icons/hi2';
 
 export default function Order({
   elem,
@@ -21,15 +22,42 @@ export default function Order({
     if (status === 'delivered') return 'bg-gray-200 text-gray-700';
     if (status === 'canceled') return 'bg-red-200 text-red-700';
     if (status === 'received') return 'bg-yellow-200 text-yellow-700';
+    if (status === 'paid') return 'bg-purple-200 text-purplu-700';
   };
 
   return (
     order && (
-      <section className="w-full md:h-36 flex flex-row flex-wrap md:flex-nowrap bg-white gap-3 border-[1px] rounded-lg p-1">
-        {/*Block image and shop name */}
-        <div className="p-1">
+      <section className="w-full  flex flex-col flex-wrap md:flex-nowrap bg-white gap-3 border-[1px] rounded-lg p-2 text-sm md:text-lg">
+        <div className="flex flex-row justify-between">
           <p className="flex justify-center">{order.promStoreId}</p>
-          <div className="max-w-28 min-w-16 max-h-28 min-h-16 object-contain">
+          <div className="flex flex-row justify-between gap-2">
+            <Link href={''} className="text-blue-700 hover:underline">
+              № {order.id}
+            </Link>
+            <div>
+              <span
+                className={clsx(
+                  'px-3 py-1 text-sm font-semibold rounded-full ',
+                  statusStyle(order.status),
+                )}
+              >
+                {order.status_name}
+              </span>
+            </div>
+          </div>
+          <p className="text-gray-500">
+            {new Date(order.date_created).toLocaleString('uk-UA', {
+              // year: 'numeric',
+              // month: '2-digit',
+              // day: '2-digit',
+              hour: '2-digit',
+              minute: '2-digit',
+            })}
+          </p>
+        </div>
+        {/*Block image and shop name */}
+        <div className="flex flex-row p-1">
+          <div className="max-w-28 min-w-16 max-h-28 min-h-16 object-contain p-1">
             <Image
               className="rounded-lg object-contain w-full h-full"
               src={order.products[0].image}
@@ -38,48 +66,34 @@ export default function Order({
               height={100}
             />
           </div>
-        </div>
-        {/*Block info */}
-        <div className="flex flex-col md:flex-row w-[265px] md:w-64 p-1">
-          <div>
-            <div className="flex flex-row justify-between">
-              <Link href={''} className="text-blue-700 hover:underline">
-                № {order.id}
-              </Link>
-              <div>
-                <span
-                  className={clsx(
-                    'px-3 py-1 text-sm font-semibold rounded-full ',
-                    statusStyle(order.status),
-                  )}
-                >
-                  {order.status_name}
-                </span>
-              </div>
-            </div>
-            <p className="text-gray-500">
-              {new Date(order.date_created).toLocaleString('uk-UA', {
-                year: 'numeric',
-                month: '2-digit',
-                day: '2-digit',
-                hour: '2-digit',
-                minute: '2-digit',
-              })}
-            </p>
+          {/*Block info */}
+          <div className="flex flex-col justify-between gap-3 md:flex-row w-full p-1">
+            {/* <div> */}
             <p
               className={clsx(
                 styles.productText,
-                'overflow-hidden overflow-ellipsis',
+                'overflow-hidden overflow-ellipsis flex-1 min-w-0',
               )}
             >
               {order.products[0].name_multilang.uk}
             </p>
-          </div>
-          {/*Price block */}
-          <div className="flex justify-between md:flex-col">
-            <p>{order.full_price}</p>
-            <p>{order.products.length}шт.</p>
-            <p>{order.cpa_commission.amount}</p>
+            {/* </div> */}
+            {/*Price block */}
+            <div className="flex justify-between md:flex-col">
+              <p>
+                <span className="inline-flex items-center gap-1 px-3 py-1 text-sm font-semibold rounded-full bg-blue-100 ">
+                  <HiOutlineShoppingCart />
+                  {order.full_price}
+                </span>
+              </p>
+              {/* <p>{order.products.length}шт.</p> */}
+              <p>
+                <span className="inline-flex items-center gap-1 px-3 py-1 text-sm font-semibold rounded-full bg-slate-100">
+                  <HiOutlinePercentBadge />
+                  {order.cpa_commission.amount} грн
+                </span>
+              </p>
+            </div>
           </div>
         </div>
         <button className="m-auto" onClick={handleDetails}>
